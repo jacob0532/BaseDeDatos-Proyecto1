60dc54c2c9d2f5b4228e62230bf45927a3f758e8
Darío Vargas <dariovm13@gmail.com>
2020-10-28 18:44:55 -0600

    Se separaron los scripts

--- Script  no catalogos.sql
+++ Script  no catalogos.sql
@@ -1,5 +1,5 @@
+-------------Datos de no catalogos---------------
 --Se lee el archivo XML
-USE ProyectoBD1
 DECLARE @xmlData XML
 SET @xmlData = ( 
 	SELECT * FROM OPENROWSET(
@@ -7,78 +7,6 @@ SET @xmlData = (
 	) AS xmlData
 )
 
---Inserta el tipo de documento de indentidad de los xml
-	INSERT INTO TipoDocIdentidad(ID, NOMBRE)
-
-	SELECT 
-		ref.value('@Id', 'int'),
-		ref.value('@Nombre', 'VARCHAR(100)')     
-
-	FROM @xmlData.nodes('Datos/Tipo_Doc/TipoDocuIdentidad') 
-	xmlData( ref )
-
-	WHERE ref.value('@Id', 'int') not in(select id from TipoDocIdentidad)
-
---Inserta los tipos de moneda
-	INSERT INTO TipoMoneda(
-		id, 
-		Nombre, 
-		Simbolo)
-
-	SELECT
-		ref.value('@Id', 'int'),
-		ref.value('@Nombre', 'VARCHAR(100)'),
-		ref.value('@Simbolo', 'char(10)')
-
-	FROM @xmlData.nodes('Datos/Tipo_Moneda/TipoMoneda')
-	xmlData( ref )
-
-	WHERE ref.value('@Id', 'int') not in(select id from TipoMoneda)
-
---Inserta los datos del parentezco
-	INSERT INTO Parentezco(ID, NOMBRE)
-
-	SELECT
-		ref.value('@Id', 'int'),
-		ref.value('@Nombre', 'VARCHAR(100)') 
-
-	FROM @xmlData.nodes('Datos/Parentezcos/Parentezco')
-	xmlData( ref )
-
-	WHERE ref.value('@Id', 'int') not in(select id from Parentezco)
-	
---Inserta los datos de tipo de cuenta ahorro
-	INSERT INTO TipoCuentaAhorro(
-		id,
-		Nombre,
-		IdTipoMoneda,
-		SaldoMinimo,
-		MultaSaldoMin, 
-		CargoAnual,
-		NumRetirosHumano, 
-		NumRetirosAutomatico, 
-		ComisionHumano, 
-		ComisionAutomatico, 
-		Interes)
-
-	SELECT
-		ref.value('@Id', 'int'),
-		ref.value('@Nombre', 'varchar(100)'),
-		ref.value('@IdTipoMoneda', 'int'),
-		ref.value('@SaldoMinimo', 'money'),
-		ref.value('@MultaSaldoMin', 'float'),
-		ref.value('@CargoAnual', 'float'),
-		ref.value('@NumRetirosHumano', 'int'),
-		ref.value('@NumRetirosAutomatico', 'int'),
-		ref.value('@ComisionHumano', 'int'),
-		ref.value('@ComisionAutomatico', 'int'),
-		ref.value('@Interes', 'int')	
-
-	FROM @xmlData.nodes('Datos/Tipo_Cuenta_Ahorros/TipoCuentaAhorro')
-	xmlData( ref )
-	WHERE ref.value('@Id', 'int') not in(select id from TipoCuentaAhorro)
-
--------------Datos de no catalogos---------------
 --Inserta los datos de las personas
 	INSERT INTO Cliente (	
 		Nombre, 
@@ -185,15 +113,3 @@ SET @xmlData = (
 	FROM @xmlData.nodes('Datos/Usuarios_Ver/UsuarioPuedeVer ')
 	xmlData( ref )
 	WHERE ref.value('@NumeroCuenta', 'int') not in(select NumeroCuenta from UsuarioPuedeVer)
-
-
-	Select * from TipoDocIdentidad
-	Select * from TipoMoneda	
-	Select * from Parentezco
-	Select * from TipoCuentaAhorro
-	Select * from Cliente
-	Select * from CuentaAhorro	-- no esta insertando
-	Select * from Beneficiarios -- no esta insertando
-	Select * from EstadoCuenta	-- no esta insertando 
-	Select * from Usuario
-	Select * from UsuarioPuedeVer
---
+++ Script catalogos.sql
@@ -0,0 +1,80 @@
+--Se lee el archivo XML
+DECLARE @xmlData XML
+SET @xmlData = ( 
+	SELECT * FROM OPENROWSET(
+		BULK 'D:\s3\Datos_Tarea1.xml',SINGLE_BLOB
+	) AS xmlData
+)
+
+--Inserta el tipo de documento de indentidad de los xml
+	INSERT INTO TipoDocIdentidad(
+		ID, 
+		NOMBRE)
+
+	SELECT 
+		ref.value('@Id', 'int'),
+		ref.value('@Nombre', 'VARCHAR(100)')     
+
+	FROM @xmlData.nodes('Datos/Tipo_Doc/TipoDocuIdentidad') 
+	xmlData( ref )
+
+	WHERE ref.value('@Id', 'int') not in(select id from TipoDocIdentidad)
+
+--Inserta los tipos de moneda
+	INSERT INTO TipoMoneda(
+		id, 
+		Nombre, 
+		Simbolo)
+
+	SELECT
+		ref.value('@Id', 'int'),
+		ref.value('@Nombre', 'VARCHAR(100)'),
+		ref.value('@Simbolo', 'char(10)')
+
+	FROM @xmlData.nodes('Datos/Tipo_Moneda/TipoMoneda')
+	xmlData( ref )
+
+	WHERE ref.value('@Id', 'int') not in(select id from TipoMoneda)
+
+--Inserta los datos del parentezco
+	INSERT INTO Parentezco(ID, NOMBRE)
+
+	SELECT
+		ref.value('@Id', 'int'),
+		ref.value('@Nombre', 'VARCHAR(100)') 
+
+	FROM @xmlData.nodes('Datos/Parentezcos/Parentezco')
+	xmlData( ref )
+
+	WHERE ref.value('@Id', 'int') not in(select id from Parentezco)
+	
+--Inserta los datos de tipo de cuenta ahorro
+	INSERT INTO TipoCuentaAhorro(
+		id,
+		Nombre,
+		IdTipoMoneda,
+		SaldoMinimo,
+		MultaSaldoMin, 
+		CargoAnual,
+		NumRetirosHumano, 
+		NumRetirosAutomatico, 
+		ComisionHumano, 
+		ComisionAutomatico, 
+		Interes)
+
+	SELECT
+		ref.value('@Id', 'int'),
+		ref.value('@Nombre', 'varchar(100)'),
+		ref.value('@IdTipoMoneda', 'int'),
+		ref.value('@SaldoMinimo', 'money'),
+		ref.value('@MultaSaldoMin', 'float'),
+		ref.value('@CargoAnual', 'float'),
+		ref.value('@NumRetirosHumano', 'int'),
+		ref.value('@NumRetirosAutomatico', 'int'),
+		ref.value('@ComisionHumano', 'int'),
+		ref.value('@ComisionAutomatico', 'int'),
+		ref.value('@Interes', 'int')	
+
+	FROM @xmlData.nodes('Datos/Tipo_Cuenta_Ahorros/TipoCuentaAhorro')
+	xmlData( ref )
+	WHERE ref.value('@Id', 'int') not in(select id from TipoCuentaAhorro)
