b8ddec0a61cfa496b09dd0bb30382b0fbdf3ca00
Darío Vargas <dariovm13@gmail.com>
2020-11-12 19:29:26 -0600

    Mejoras en validaciones del sp editar beneficiarios.

--- SP/SP editar Beneficiarios.sql
+++ SP/SP editar Beneficiarios.sql
@@ -1,65 +1,75 @@
+USE [ProyectoBD1]
+GO
+/****** Object:  StoredProcedure [dbo].[EditarBeneficiarios]    Script Date: 11/12/2020 7:18:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 /*Procedimiento que edita los beneficiarios*/
-ALTER PROCEDURE EditarBeneficiarios @inNumeroCuenta INT
+ALTER PROCEDURE [dbo].[EditarBeneficiarios]
+	@inNumeroCuenta INT
 	,@inValorDocIdentidadBeneficiario INT
 	,@inParentezcoId INT
 	,@inPorcentaje INT
 	,@inAccion VARCHAR(10)
+
 AS
+DECLARE @outErrorCode int = 0
 BEGIN
 	SET NOCOUNT ON
-
-	/*Validaciones*/
-	IF @inNumeroCuenta IN (
-			SELECT 1
-			FROM CuentaAhorro
-			WHERE NumeroCuenta = @inNumeroCuenta
-			)
-		RETURN 5001	--El numero de cuenta no existe
-
-	IF @inValorDocIdentidadBeneficiario IN (
-			SELECT 1
-			FROM Cliente
-			WHERE ValorDocIdentidad = @inValorDocIdentidadBeneficiario
-			)
-		RETURN 5002 --el beneficiario no existe
-
-	IF @inParentezcoId NOT IN (
-			SELECT 1
-			FROM Parentezco
-			WHERE id = @inParentezcoId
-			)
-		RETURN 5002	--El parentezco no existe
-
-	IF @inPorcentaje < 0
-		OR @inPorcentaje > 100
-		RETURN 5003	--El porcentaje es mayor a cien o negativo
-
 	/*Agrega un nuevo beneficiario*/
 	IF @inAccion = 'AGREGAR'
 	BEGIN
-		INSERT INTO Beneficiarios (
-			NumeroCuenta
-			,ValorDocumentoIdentidadBeneficiario
-			,ParentezcoId
-			,Porcentaje
-			)
-		VALUES (
-			@inNumeroCuenta
-			,@inValorDocIdentidadBeneficiario
-			,@inParentezcoId
-			,@inPorcentaje
-			)
-	END
+		IF NOT EXISTS (SELECT NumeroCuenta FROM CuentaAhorro WHERE NumeroCuenta = @inNumeroCuenta)
+			SET @outErrorCode = 5001	--El numero de cuenta no existe
+	
+		IF NOT EXISTS (SELECT ValorDocIdentidad FROM Cliente WHERE ValorDocIdentidad = @inValorDocIdentidadBeneficiario )
+			SET @outErrorCode = 5002	--El beneficiario no existe
+
+		IF (SELECT COUNT(*) FROM Beneficiarios where NumeroCuenta = @inNumeroCuenta) > 3
+			Set @outErrorCode = 5005	--Posee más de 3 beneficiarios
+
+		IF @inPorcentaje < 0 OR @inPorcentaje > 100
+			SET @outErrorCode = 5004	--El porcentaje es mayor o igual 100 o negativo
+
+		IF EXISTS(SELECT ValorDocumentoIdentidadBeneficiario from Beneficiarios where @inValorDocIdentidadBeneficiario = ValorDocumentoIdentidadBeneficiario)
+			Set @outErrorCode = 5006	--El Beneficiario ya existe
+
+		ELSE
+				INSERT INTO Beneficiarios (
+					NumeroCuenta
+					,ValorDocumentoIdentidadBeneficiario
+					,ParentezcoId
+					,Porcentaje
+					)
+				VALUES (
+					@inNumeroCuenta
+					,@inValorDocIdentidadBeneficiario
+					,@inParentezcoId
+					,@inPorcentaje
+					)
+	END	
 
 	/*Editar beneficiario*/
 	IF @inAccion = 'EDITAR'
 	BEGIN
-		UPDATE Beneficiarios
-		SET NumeroCuenta = @inNumeroCuenta
-			,ValorDocumentoIdentidadBeneficiario = @inValorDocIdentidadBeneficiario
-			,ParentezcoId = @inParentezcoId
-			,Porcentaje = @inPorcentaje
-		WHERE NumeroCuenta = @inNumeroCuenta
+		IF NOT EXISTS (SELECT ValorDocIdentidad FROM Cliente WHERE ValorDocIdentidad = @inValorDocIdentidadBeneficiario )
+			SET @outErrorCode = 5002 --El beneficiario no existe
+
+		IF NOT EXISTS(SELECT id FROM Parentezco WHERE id = @inParentezcoId)
+			SET @outErrorCode = 5003	--El parentezco no existe
+
+		IF @inPorcentaje < 0 OR @inPorcentaje > 100
+			SET @outErrorCode = 5004	--El porcentaje es mayor o igual 100 o negativo
+
+		IF  EXISTS(SELECT ValorDocumentoIdentidadBeneficiario from Beneficiarios where ValorDocumentoIdentidadBeneficiario = @inValorDocIdentidadBeneficiario)
+			Set @outErrorCode = 5006	--El Beneficiario ya existe
+		ELSE
+			UPDATE Beneficiarios
+			SET ValorDocumentoIdentidadBeneficiario = @inValorDocIdentidadBeneficiario
+				,ParentezcoId = @inParentezcoId
+				,Porcentaje = @inPorcentaje
+			WHERE NumeroCuenta = @inNumeroCuenta
 	END
 
 	/*Eliminar beneficiario*/
@@ -71,4 +81,5 @@ BEGIN
 			WHERE NumeroCuenta = @inNumeroCuenta
 		END
 	SET NOCOUNT OFF
+	RETURN @outErrorCode
 END;
